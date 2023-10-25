8cd748742c00d808326a07a3e84f8a5fd5369b58
sofia <spardo1@ufl.edu>
2023-10-25 18:02:59 -0400

    added comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def encode_pass(password):
+def encode_pass(password):                                  # function encoded password by adding 3 to each digit
     pass_list = list(password)
     en_pass = list()
     for i in pass_list:
@@ -9,7 +9,7 @@ def encode_pass(password):
     return en_pass
 
 
-def decode(enc_password):
+def decode(enc_password):                               # function decodes password by subtracting 3 from each digit
     list_encpassword = list(enc_password)
     for i in range(0, len(enc_password)):
         list_encpassword[i] = str(int(enc_password[i]) - 3)
@@ -17,7 +17,7 @@ def decode(enc_password):
     return "".join(list_encpassword)
 
 
-def menu():
+def menu():                                             # function to display menu
     print('\nMenu\n' +
           '-------------\n' +
           '1. Encode\n' +
@@ -29,13 +29,13 @@ if __name__ == '__main__':
     user_input = 0
     while True:
         menu()
-        user_input = input('Please enter an option: ')
+        user_input = input('Please enter an option: ')      # asks for user option
         if user_input == '1':
             user_pass = input('Please enter your password to encode: ')
             encoded = encode_pass(user_pass)
             print('Your password has been encoded and stored!')
         if user_input == '2':
-            print('The encoded password is, ' + encoded + ' and the original password is ' + decode(encoded) + '.')
+            print('The encoded password is ' + encoded + ', and the original password is ' + decode(encoded) + '.')
         if user_input == '3':
-            break
+            break                                           # ends program
 
